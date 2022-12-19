2240dd41e49dd7aacdf8b1232e6e58d012494cd1
Roman Zipp <ich@ich.wtf>
2022-12-19 10:57:54 +0100

    Generate model docs

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -4,6 +4,20 @@ namespace StoreNotifier\Models;
 
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property int $id
+ * @property string $provider
+ * @property string $store_product_id
+ * @property string $title
+ * @property string $url
+ * @property string|null $image_url
+ * @property string $last_checked_at
+ * @property string|null $published_at
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property \Illuminate\Database\Eloquent\Collection|\StoreNotifier\Models\Variant[] $variants
+ * @property int|null $variants_count
+ */
 class Product extends AbstractModel
 {
     public $table = 'products';
--- src/Models/Variant.php
+++ src/Models/Variant.php
@@ -2,6 +2,16 @@
 
 namespace StoreNotifier\Models;
 
+/**
+ * @property int $id
+ * @property int $product_id
+ * @property string $store_variant_id
+ * @property string $title
+ * @property int $price
+ * @property bool $available
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ */
 class Variant extends AbstractModel
 {
     protected $table = 'variants';
