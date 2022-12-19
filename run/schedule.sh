814a971aa57f23a8b81ee3819fa5874223adf5b3
Roman Zipp <ich@ich.wtf>
2022-12-19 12:41:57 +0100

    Add scheduler script

---
+++ run/schedule.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+while true
+do
+  printf '%s' "executing... "
+  start=`date +%s`;
+  php $(pwd)/app.php
+  end=`date +%s`;
+  diff=$(expr $end - $start)
+  printf '%s' "executed in $diff seconds"
+  echo ""
+  sleep 60
+
+done
