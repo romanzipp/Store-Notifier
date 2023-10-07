119a25c1bfcf2068f2b75cfb94d9d3b696e1a6c6
Roman Zipp <ich@ich.wtf>
2023-10-08 01:59:22 +0200

    Update mpb products

--- src/Providers/MpbProvider.php
+++ src/Providers/MpbProvider.php
@@ -62,29 +62,35 @@ class MpbProvider extends AbstractProvider
         EOF;
 
         $products = [
-            new ProductData([
-                'store_product_id' => '67104',
-                'title' => 'Sony Alpha A7 III',
-                'url' => 'https://www.mpb.com/de-de/produkt/sony-alpha-a7-iii?sort[productLastOnline]=DESC',
-                'image_url' => 'https://www.mpb.com/cdn-cgi/image/width=286,quality=90,format=jpeg/media-service/90b1fe0e-9369-483b-8ce7-5d9f0acea421',
-            ]),
-            new ProductData([
-                'store_product_id' => '66765',
-                'title' => 'Sony Alpha A7R III',
-                'url' => 'https://www.mpb.com/de-de/produkt/sony-alpha-a7r-iii?sort[productLastOnline]=DESC',
-                'image_url' => 'https://www.mpb.com/media-service-img-cdn/width=286,quality=90,format=jpeg/media-service/bd3c6992-07bb-4910-b82e-9af05997e783',
-            ]),
+            // new ProductData([
+            //     'store_product_id' => '67104',
+            //     'title' => 'Sony Alpha A7 III',
+            //     'url' => 'https://www.mpb.com/de-de/produkt/sony-alpha-a7-iii?sort[productLastOnline]=DESC',
+            //     'image_url' => 'https://www.mpb.com/cdn-cgi/image/width=286,quality=90,format=jpeg/media-service/90b1fe0e-9369-483b-8ce7-5d9f0acea421',
+            // ]),
+            // new ProductData([
+            //     'store_product_id' => '66765',
+            //     'title' => 'Sony Alpha A7R III',
+            //     'url' => 'https://www.mpb.com/de-de/produkt/sony-alpha-a7r-iii?sort[productLastOnline]=DESC',
+            //     'image_url' => 'https://www.mpb.com/media-service-img-cdn/width=286,quality=90,format=jpeg/media-service/bd3c6992-07bb-4910-b82e-9af05997e783',
+            // ]),
             new ProductData([
                 'store_product_id' => '68774',
-                'title' => 'Sigma 24-70mm f/2.8 DG DN ART',
+                'title' => 'Sigma 24-70mm f/2.8',
                 'url' => 'https://www.mpb.com/de-de/produkt/sigma-24-70mm-f28-dg-dn-art-sony-e-fit?sort[productLastOnline]=DESC',
                 'image_url' => 'https://www.mpb.com/media-service-img-cdn/width=286,quality=90,format=jpeg/media-service/c2c3158e-0c1e-44c7-b133-a47adc69b50d',
             ]),
+            // new ProductData([
+            //     'store_product_id' => '70585',
+            //     'title' => 'Sigma 28-70mm f/2.8 DG DN',
+            //     'url' => 'https://www.mpb.com/de-de/produkt/sigma-28-70mm-f28-dg-dn-contemporary-sony-e-fit?sort[productLastOnline]=DESC',
+            //     'image_url' => 'https://www.mpb.com/media-service-img-cdn/width=286,quality=90,format=jpeg/media-service/12d5b913-9754-4612-bc5d-acab1cb56c40',
+            // ]),
             new ProductData([
-                'store_product_id' => '70585',
-                'title' => 'Sigma 28-70mm f/2.8 DG DN',
-                'url' => 'https://www.mpb.com/de-de/produkt/sigma-28-70mm-f28-dg-dn-contemporary-sony-e-fit?sort[productLastOnline]=DESC',
-                'image_url' => 'https://www.mpb.com/media-service-img-cdn/width=286,quality=90,format=jpeg/media-service/12d5b913-9754-4612-bc5d-acab1cb56c40',
+                'store_product_id' => '70832',
+                'title' => 'Sigma 150-600mm f/5-6.3',
+                'url' => 'https://www.mpb.com/de-de/produkt/sigma-150-600mm-f5-63-dg-dn-os-sport-sony-fe-fit?sort[productLastOnline]=DESC',
+                'image_url' => 'https://www.mpb.com/cdn-cgi/image/width=286,quality=90,format=jpeg/media-service/2d799831-2981-477b-bc56-223e62bef82e',
             ]),
         ];
 
